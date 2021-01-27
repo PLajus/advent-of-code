c16599f67e46603a7eb775c336663237e032b082
Povilas Lajus <povilas.lajus@gmail.com>
2021-01-27 19:17:30 +0200

    Terminated outter loop answer is found

--- day1/reportrepair.py
+++ day1/reportrepair.py
@@ -1,4 +1,4 @@
-my_file = open("D:\\Coding\\.vscode\\day1\\input.txt", "r")
+my_file = open("D:\\Coding\\AdventofCode\\day1\\input.txt", "r")
 datalist = my_file.read().split()
 datalist = list(map(int, datalist))
 my_file.close()
@@ -7,7 +7,7 @@ my_file.close()
 for i in range(0, len(datalist) - 1):
     for j in range(i + 1, len(datalist)):
         if datalist[i] + datalist[j] == 2020:
-            print(datalist[i] * datalist[j])
+            print("Part 1 answer: ", datalist[i] * datalist[j])
             break
 
 # Part 1 better solution
@@ -19,8 +19,14 @@ for i in range(0, len(datalist) - 1):
 #  s.add(int(datalist[i]))
 
 # Part 2
-for i in range(len(datalist)):
+for i in range(len(datalist) - 1):
     for j in range(i + 1, len(datalist)):
         if 2020 - (datalist[i] + datalist[j]) in datalist:
-            print(datalist[i] * datalist[j] * (2020 - (datalist[i] + datalist[j])))
+            print(
+                "Part 2 answer: ",
+                datalist[i] * datalist[j] * (2020 - (datalist[i] + datalist[j])),
+            )
             break
+    else:
+        continue
+    break
