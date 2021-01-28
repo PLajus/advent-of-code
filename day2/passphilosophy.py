9855cf26da46d7927d029b8a51b15ca8da5b236b
Povilas Lajus <povilas.lajus@gmail.com>
2021-01-28 19:59:15 +0200

    Part 2 solution

--- day2/passphilosophy.py
+++ day2/passphilosophy.py
@@ -1,6 +1,4 @@
-# input_file = open("D:\\Coding\\AdventofCode\\day2\\input.txt", "r")
-# inputdata = input_file.read()  # .splitlines()
-
+# Part 1
 count = 0
 with open("D:\\Coding\\AdventofCode\\day2\\input.txt", "r") as f:
     for line in f:
@@ -8,6 +6,16 @@ with open("D:\\Coding\\AdventofCode\\day2\\input.txt", "r") as f:
         num1, num2 = map(int, interval.split("-"))
         if password.count(char[0]) >= num1 and password.count(char[0]) <= num2:
             count += 1
-
 print(count)
-# gonna try using something like grep
+
+# Part 2
+count2 = 0
+with open("D:\\Coding\\AdventofCode\\day2\\input.txt", "r") as f:
+    for line in f:
+        interval, char, password = line.strip().split()
+        num1, num2 = map(int, interval.split("-"))
+        if password[num1 - 1] == char[0] and password[num2 - 1] != char[0]:
+            count2 += 1
+        elif password[num1 - 1] != char[0] and password[num2 - 1] == char[0]:
+            count2 += 1
+print(count2)
